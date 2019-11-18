1ca34b966bedb09cbe6dee3510a79d057f268dd4
Zoe O <zoeo@Zoes-MBP.fios-router.home>
2019-11-17 20:12:48 -0500

    idk how to do this

--- src/sum.js
+++ src/sum.js
@@ -1,9 +1,11 @@
 const readlineSync = require("readline-sync");
-const lowerBound = Number(readlineSync.question("Lower Bound: "));
-const upperBound = Number(readlineSync.question("Upper Bound: "));
+const min = Number.MIN_SAFE_INTEGER;
+const max = Number.MAX_SAFE_INTEGER;
+let lowerBound = Number(readlineSync.question("Lower Bound: "));
+let upperBound = Number(readlineSync.question("Upper Bound: "));
+let sum = 0;
 
-if(Number.isNaN(lowerBound))||(Number.isNaN(upperBound)){
-   console.log("Invalid.");
-}else if(lowerBound < Number.MIN_SAFE_INTEGER || upperBound < Number.MIN_SAFE_INTEGER && lowerBound > Number.MAX_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTEGER) {
-     console.log("\nInvalid.");
+while(Number.isNaN(lowerBound)|| Number.isNaN(upperBound)|| !Number.isInteger(lowerBound)|| !Number.isInteger(upperBound) || !Number.MIN_SAFE_INTEGER<lowerBound<Number.MAX_SAFE_INTEGER|| !Number.MIN_SAFE_INTEGER<upperBound<Number.MAX_SAFE_INTEGER){
+    lowerBound = Number(readlineSync.question("Lower bound: "));
+    upperBound = Number(readlineSync.question("Upper bound: "));
 }
