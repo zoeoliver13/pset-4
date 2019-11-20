bf65133d6caac203b73c147d5c885abb07927e7e
Zoe O <zoeo@Zoes-MBP.fios-router.home>
2019-11-20 16:33:20 -0500

    idk

--- src/average.js
+++ src/average.js
@@ -6,3 +6,8 @@ let positiveInteger = Number(readlineSync.question("\nNon-negative integer: "));
 while(Number.isNaN(positiveInteger)|| !Number.isInteger(positiveInteger)|| !min>positiveInteger|| !positiveInteger>max){
   positiveInteger = Number(readlineSync.question("Non-negative integer: "));
 }
+while (positiveInteger !>=0) {
+  let average;
+  average = positiveInteger++
+  average/positiveInteger
+}
