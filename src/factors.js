9f5d982b7ec5c3e309ae8f189d3fbd595bd1026e
Zoe Oliver <ZOliver@ucvts.tec.nj.us>
2019-11-22 10:45:22 -0500

    mario

--- src/factors.js
+++ src/factors.js
@@ -1,18 +1,15 @@
 const readlineSync = require("readline-sync");
 const MIN = 1;
 const MAX = Number.MAX_SAFE_INTEGER;
-let positiveInteger = Number(readlineSync.question("\nPositive integer: "));
-while(positiveInteger <= MIN || positiveInteger >= MAX||Number.isNaN(positiveInteger) || !Number.isInteger(positiveInteger )){
-  positiveInteger = Number(readlineSync.question("Positive integer: "));
+let integer = 0;
+let str = "";
+let number = 2;
+while(integer <= MIN || integer >= MAX||Number.isNaN(integer) || !Number.isInteger(integer)){
+  integer = Number(readlineSync.question("Positive integer: "));
 }
-let first = [];
-let second = 0
-  for(let i = 1; i < positiveInteger/2; i++){
-    if(positiveInteger % i == 0){
-      second = positiveInteger/i;
-      first.push(i);
-      first.push(second);
-    }
-  }
-first = [...new Set(first)];
-console.log("\n"+ first.join(", ")+".\n");
+
+while(integer%number==0){
+  str = str + number;
+  number++
+}
+console.log(number);
--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,14 +1,19 @@
 const readlineSync = require("readline-sync");
 const MIN = 1;
 const MAX = 78;
-let number = Number(readlineSync.question("\nPositive integer: "));
-while(number < MIN || positiveInteger > MAX||Number.isNaN(number) || !Number.isInteger(number)){
-  positiveInteger = Number(readlineSync.question("Positive integer: "));
+let number=0;
+let previousnum=0;
+let fibonacci;
+let int = 1;
+console.log("");
+while(number < MIN || number > MAX||Number.isNaN(number) || !Number.isInteger(number)){
+  number = Number(readlineSync.question("Positive integer: "));
 }
-let fibonacci = [];
-fibonacci[0] = 0;
-fibonacci[1] = 1;
-for (let number = 2; number <= 10; number++) {
-    fibonacci[number] = fibonacci[number - 2] + fibonacci[number - 1];
-  }
-  console.log(fibonacci[number]);
+for(let i = 2; i <= number; i++){
+  fibonacci = previousnum + int;
+  previousnum = int;
+  int = fibonacci;
+
+}
+fibonacci = fibonacci.toLocaleString("en");
+console.log("\n"+fibonacci+".\n");
